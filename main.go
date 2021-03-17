9a872ffbab9db8cb1ac7451febdaabe9945e8c63
Michael <michaaka@stud.ntnu.no>
2021-03-17 13:16:08 +0100

    Add initial template

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
