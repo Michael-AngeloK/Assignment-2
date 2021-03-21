b2edca014e93e06581fb74246043136daba53d3c
Michael <michaaka@stud.ntnu.no>
2021-03-21 16:38:17 +0100

    Added covidStingency.go for stingency handling

---
+++ corona/covidStingency.go
@@ -0,0 +1,17 @@
+package corona
+
+import (
+	//"encoding/json"
+	"net/http"
+	//"net/url"
+	//"strconv"
+	//"strings"
+)
+
+//
+type coronaStingency struct {
+}
+
+//
+func HandlerCoronaStingency(w http.ResponseWriter, r *http.Request) {
+}
