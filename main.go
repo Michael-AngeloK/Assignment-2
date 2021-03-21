0460e0f25faeee69436f845c321be4fdecefe6b4
Michael <michaaka@stud.ntnu.no>
2021-03-21 16:37:09 +0100

    Tweaked main.go with corona cases instead

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"main/corona"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -11,5 +13,15 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
+	corona.StartTime = time.Now()
+	//corona Cases handler
+	http.HandleFunc("/corona/v1/country/", corona.HandlerCoronaCases)
+	//corona Stingency handler
+	http.HandleFunc("/corona/v1/policy/", corona.HandlerCoronaStingency)
+	//diagnosis handler
+	http.HandleFunc("/corona/v1/diag/", corona.HandlerDiagnosis)
+	//notifications handler
+	http.HandleFunc("/corona/v1/notifications/", corona.HandlerNotifications)
+	//ends program if it can't open port
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
