10380a0b4ffe0abaad6c4bbdb69d2ade5e041638
Michael <michaaka@stud.ntnu.no>
2021-03-21 16:40:42 +0100

    Added dataRequest.go for easily getting raw data

---
+++ corona/dataRequest.go
@@ -0,0 +1,26 @@
+package corona
+
+import (
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+//requestRawData gets raw data
+func requestRawData(url string) ([]byte, error) {
+	var err error
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	client := http.Client{
+		Timeout: time.Second * 2,
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	output, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return output, err
+}
